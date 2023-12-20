1086909f8f930c3170c1bdec0a29209dd1ea89a6
Juan Pablo Luncumilla <jpluncumilla@Juans-MacBook-Pro-2.local>
2023-12-19 20:00:57 -0500

    Setting up sections

---
+++ src/app/components/main.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Main() {
+  return (
+    <div>
+      <div>
+        <h1>JP Luncumilla</h1>
+        <p>Frontend Software Engineer</p>
+      </div>
+      <div>
+        <h1>About</h1>
+        <p>Paragraphs about the kid</p>
+      </div>
+      <div>
+        <h1>Work</h1>
+      </div>
+      <div>
+        <h1>Contact</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
